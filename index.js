d6162a07daf1f36cc204e5a3c5c9802f866b3d78
srohimah <sitirohimach@gmail.com>
2018-02-07 16:29:45 +0700

    release 1

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const snakeAndLadder = () =>{
+const snakeAndLadder = (num) =>{
     let board = []
-    for (let row=10; row>=1; row--){
+    for (let row=num; row>=1; row--){
         let rowBoard=[]
-        for (let i=1; i<=10; i++){
+        for (let i=1; i<=num; i++){
             row % 2 === 0 ?
-            rowBoard.push(10*(row-1)+(11-i)):
-            rowBoard.push(10*(row-1)+i)
+            rowBoard.push(num*(row-1)+((num+1)-i)):
+            rowBoard.push(num*(row-1)+i)
         }
         board.push(rowBoard)
     }
     return board
 }
 
-console.log(snakeAndLadder())
+console.log(snakeAndLadder(8))
