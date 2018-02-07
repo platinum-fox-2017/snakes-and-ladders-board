1cd56e8ba4d40a7e4297bb22783275cd2886dd47
unknown <fatimah.azzhr@gmail.com>
2018-02-07 18:05:40 +0700

    release 0&1

---
+++ index.js
@@ -0,0 +1,48 @@
+function generateBoard(num) {
+  let result = [];
+  let count = Math.pow(num, 2);
+ 
+  if(count%2 === 0) {
+    for (let i = 0; i < num; i++) {
+      let rows = [];
+      result.push(rows);
+      if (i%2 === 0) {
+        for (let j = 0; j < num; j++) {
+          rows.push(count);
+          count--;
+        }
+      } else if (i%2 !== 0) {
+        count -= (num - 1);
+        for (let j = 0; j < num; j++) {
+          rows.push(count);
+          count++;
+        }
+        count -= (num + 1);
+      }
+    }
+  } else if(count%2 !== 0) {
+    count -= (num-1);
+    for(let i = 0; i < num; i++) {
+      let rows = [];
+      if(i%2 === 0) {
+        for(let j = 0; j < num; j++) {
+          rows.push(count);
+          count++;
+        }
+        result.push(rows);
+        count -= (num-1);
+      } else if(i%2 !== 0) {
+        for(let j = 0; j < num; j++) {
+          rows.push(count);
+          count--;
+        }
+        result.push(rows);
+        count -= (num+1);
+      }
+    }
+  }
+
+  return result;
+}
+console.log(generateBoard(15));
+console.log(generateBoard(10));
