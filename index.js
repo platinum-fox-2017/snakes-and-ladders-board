37504b1c69648383f9933842e05b7b7e745636b3
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-02-07 15:29:14 +0700

    Update Release 0

--- index.js
+++ index.js
@@ -3,16 +3,39 @@ function generateBoard() {
     var arraySnakeBoard = [];
     var elemenBaris = 10 * 10;
     var counter = 0;
+    var unsortedBaris = [];
+    var unsortedArraySnakeBoard = [];
+
+    var j = 10 * 10;
+    while (counter < 10) {
+        unsortedBaris.push(j);
+        j = j - 1;
+        if ((j) % 10 == 0) {
+            unsortedArraySnakeBoard.push(unsortedBaris);
+            unsortedBaris = [];
+            counter = counter + 1;
+        }    
+    }
+
+    counter = 0;
 
     while (counter < 10) {
-        baris.push(elemenBaris);
-        elemenBaris = elemenBaris - 1;
-        if ((elemenBaris) % 10 == 0) {
+        if (counter % 2 == 0) {
+            for (var idx = 0; idx < 10; idx++) {
+                baris.push(unsortedArraySnakeBoard[counter][idx]);
+            }
             arraySnakeBoard.push(baris);
             baris = [];
-            counter = counter + 1;
+        } else {
+            for (var idx = 9; idx >= 0; idx--) {
+                baris.push(unsortedArraySnakeBoard[counter][idx]);
+            }
+            arraySnakeBoard.push(baris);
+            baris = [];
         }
+        counter = counter + 1;
     }
+
     return arraySnakeBoard;
 }
 
