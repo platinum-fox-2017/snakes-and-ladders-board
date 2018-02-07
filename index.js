493ae4a77adf3b217b4db88f096e827b0a004b80
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-02-07 16:43:48 +0700

    Release 1: Refactor code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function generateBoard() {
+function generateBoard(number) {
     var baris = [];
     var arraySnakeBoard = [];
     var elemenBaris = 10 * 10;
@@ -6,11 +6,11 @@ function generateBoard() {
     var unsortedBaris = [];
     var unsortedArraySnakeBoard = [];
 
-    var j = 10 * 10;
-    while (counter < 10) {
+    var j = number * number;
+    while (counter < number) {
         unsortedBaris.push(j);
         j = j - 1;
-        if ((j) % 10 == 0) {
+        if ((j) % number == 0) {
             unsortedArraySnakeBoard.push(unsortedBaris);
             unsortedBaris = [];
             counter = counter + 1;
@@ -19,25 +19,41 @@ function generateBoard() {
 
     counter = 0;
 
-    while (counter < 10) {
-        if (counter % 2 == 0) {
-            for (var idx = 0; idx < 10; idx++) {
-                baris.push(unsortedArraySnakeBoard[counter][idx]);
+    while (counter < number) {
+        if (number % 2 == 0) {
+            if (counter % 2 == 0) {
+                for (var idx = 0; idx < number; idx++) {
+                    baris.push(unsortedArraySnakeBoard[counter][idx]);
+                }
+                arraySnakeBoard.push(baris);
+                baris = [];
+            } else {
+                for (var idx = (number-1); idx >= 0; idx--) {
+                    baris.push(unsortedArraySnakeBoard[counter][idx]);
+                }
+                arraySnakeBoard.push(baris);
+                baris = [];
             }
-            arraySnakeBoard.push(baris);
-            baris = [];
+            counter = counter + 1;
         } else {
-            for (var idx = 9; idx >= 0; idx--) {
-                baris.push(unsortedArraySnakeBoard[counter][idx]);
+            if (counter % 2 != 0) {
+                for (var idx = 0; idx < number; idx++) {
+                    baris.push(unsortedArraySnakeBoard[counter][idx]);
+                }
+                arraySnakeBoard.push(baris);
+                baris = [];
+            } else {
+                for (var idx = (number-1); idx >= 0; idx--) {
+                    baris.push(unsortedArraySnakeBoard[counter][idx]);
+                }
+                arraySnakeBoard.push(baris);
+                baris = [];
             }
-            arraySnakeBoard.push(baris);
-            baris = [];
+            counter = counter + 1;
         }
-        counter = counter + 1;
     }
 
     return arraySnakeBoard;
 }
 
-console.log(generateBoard());
-
+console.log(generateBoard(15));
