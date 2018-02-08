0b94bb0f38b0141bdf2106896b00779f1641554b
WikaSilo <wikanyaa@gmail.com>
2018-02-08 14:22:23 +0700

    refactor, pengurangan if

--- index.js
+++ index.js
@@ -4,32 +4,16 @@ function generateBoard(length) {
 
   for(let i=0; i<length; i++) {
     slBoard.push([])
-    if(length % 2 == 0) {
-      if(slBoard.length % 2 == 1) {
-        for(let j=slNum; j>=(slNum-length)+1; j--) {
-          slBoard[i].push(j)
-        }
-        slNum -= length;
+    if(length % 2 == 0 && slBoard.length % 2 == 1) {
+      for(let j=slNum; j>=(slNum-length)+1; j--) {
+        slBoard[i].push(j)
       }
-      if(slBoard.length % 2 == 0) {
-        for(let k=(slNum-length)+1; k<=slNum; k++) {
-          slBoard[i].push(k)
-        }
-        slNum -= length;
-      }
+      slNum -= length;
     } else {
-      if(slBoard.length % 2 == 0) {
-        for(let j=slNum; j>=(slNum-length)+1; j--) {
-          slBoard[i].push(j)
-        }
-        slNum -= length;
+      for(let k=(slNum-length)+1; k<=slNum; k++) {
+        slBoard[i].push(k)
       }
-      if(slBoard.length % 2 == 1) {
-        for(let k=(slNum-length)+1; k<=slNum; k++) {
-          slBoard[i].push(k)
-        }
-        slNum -= length;
-      }
+      slNum -= length;
     }
   }
   return slBoard
